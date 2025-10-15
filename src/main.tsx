7bc83d8e0a9962d2b2b9c2a9c273cf9535f0ef53
Borel SONNA <borelsonna@example.com>
2025-10-16 01:15:21 +0200

    chore: reviewStructure :sparkles:

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router";
+import "./index.css";
+import "./i18n/config.ts";
 
-  import { createRoot } from "react-dom/client";
-  import App from "./App";
-  import "./index.css";
-  import "./i18n/config.ts";
-
-  createRoot(document.getElementById("root")!).render(<App />);
-  
+createRoot(document.getElementById("root")!).render(
+  <RouterProvider router={router} />
+);
